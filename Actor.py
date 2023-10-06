f64687835e1e6ad0c6a8a571f19d20eefee48caf
gustaleitcor <gustavo.leitecordeiro@gmail.com>
2023-10-06 16:50:06 -0300

    adding Actor and Director classes

---
+++ Actor.py
@@ -0,0 +1,5 @@
+class Actor:
+    def __init__(self, name):
+        self.name = name
+        self.movies = []
+        self.avgGross = 0
---
+++ Director.py
@@ -0,0 +1,4 @@
+class Director:
+    def __init__(self, name):
+        self.name = name
+        self.movies = []
